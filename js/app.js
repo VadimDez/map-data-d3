f9d26b5523baef48af7106bc93d9412f4d33e841
Vadym Yatsyuk <vasp3d@gmail.com>
2016-01-13 22:56:54 +0100

    minor

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 /**
  * Created by vadimdez on 13/01/16.
  */
+(function () {
+
+  getData(drawMap);
+
+  /**
+   * Get data
+   * @param fn
+   */
+  function getData(fn) {
+    var url = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json';
+    d3.json(url, function (err, data) {
+      if (err) {
+        console.warn(err);
+        return;
+      }
+
+      fn(data.features);
+    });
+  }
+
+  function drawMap(data) {
+
+  }
+}());
