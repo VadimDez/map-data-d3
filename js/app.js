d68a7dbef8690d1142fb3062744d6c7ae493b2a4
Vadym Yatsyuk <vasp3d@gmail.com>
2016-01-14 23:21:57 +0100

    added map, and points

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
  */
 (function () {
 
-  getData(drawMap);
+  getData(draw);
 
   /**
    * Get data
@@ -21,7 +21,41 @@
     });
   }
 
-  function drawMap(data) {
+  function draw(data) {
+    var map = drawMap();
+    map.bubbles(getPoints(data), {
+      popupTemplate: function(geo, data) {
+        return '<div class="hoverinfo">' + JSON.stringify(data) + '</div>'
+      }
+    });
+  }
 
+  function drawMap() {
+    return new Datamap({
+      element: document.getElementById('container'),
+      geographyConfig: {
+        highlightOnHover: false,
+        popupOnHover: false
+      }
+    });
+  }
+
+  function getPoints(data) {
+    return data.map(function (object) {
+      console.log(object.geometry);
+
+      if (!object.geometry) {
+        return false;
+      }
+      return {
+        name: object.properties.name,
+        radius: 10,//object.properties.mass,
+        //yeild: 15000,
+        //significance: 'First dry fusion fuel "staged" thermonuclear weapon; a serious nuclear fallout accident occurred',
+        date: object.properties.year,
+        latitude: object.geometry.coordinates[0],
+        longitude: object.geometry.coordinates[1]
+      }
+    })
   }
 }());
