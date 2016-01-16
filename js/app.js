a9fd9e92104a6e2d91fef16418b30acabd1e3b21
Vadym Yatsyuk <vasp3d@gmail.com>
2016-01-16 15:09:17 +0100

    updated tooltip

--- js/app.js
+++ js/app.js
@@ -24,9 +24,7 @@
   function draw(data) {
     var map = drawMap();
     map.bubbles(getPoints(data), {
-      popupTemplate: function(geo, data) {
-        return '<div class="hoverinfo">' + JSON.stringify(data) + '</div>'
-      }
+      popupTemplate: getTooltip
     });
   }
 
@@ -50,11 +48,24 @@
       }
       return {
         name: object.properties.name,
+        fall: object.properties.fall,
+        recclass: object.properties.recclass,
+        reclat: object.properties.reclat,
+        nametype: object.properties.nametype,
         radius: 10,
         date: object.properties.year,
         latitude: object.geometry.coordinates[1],
         longitude: object.geometry.coordinates[0]
       }
     })
+  }
+
+  function getTooltip(geo, data) {
+    var info = '';
+    for (var key in data) {
+      info += '<div>' + key + ': ' + data[key] + '</div>';
+    }
+
+    return '<div class="hoverinfo">' + info + '</div>'
   }
 }());
